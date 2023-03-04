f2e0c8a8f0b0eff82064a78a8376bcf886510695
andre059 <andreyperm159@mail.ru>
2023-03-04 15:27:54 +0500

    1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Product:
-    storing_instances = 0
+    all = []
     discount = 0.15
 
     def __init__(self, product, product_price, quantity_goods):
@@ -7,17 +7,17 @@ class Product:
         self.product_price = product_price
         self.quantity_goods = quantity_goods
         self.product_discount = None
+        self.all.append(self)
 
     def price_product_in_store(self):
-        self.product_price = self.product_price * self.quantity_goods
-        return self.product_price
+        return self.product_price * self.quantity_goods
 
     def price_product_discount(self):
-        self.product_discount = (self.product_price / self.quantity_goods) * self.discount
+        self.product_discount = self.product_price * self.discount
 
 
 pro_1 = Product("Смартфон", 10000, 20)
-pro_2 = Product("Смартфон", 20000, 5)
+pro_2 = Product("Ноутбук", 20000, 5)
 
 print(pro_1.price_product_in_store())
 print(pro_2.price_product_in_store())
