042e675ade83d19792c1886690e984fcb1aa8854
andre059 <andreyperm159@mail.ru>
2023-03-07 14:13:49 +0500

    commit 1

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 class Product:
     all = []
-    discount = 0.15
+    discount = 0.15  # Скидка на товар 0,15 == 15%
 
     def __init__(self, product, product_price, quantity_goods):
+        """
+        Поступление данных о таваре в магазине
+        :param product: Наименование товара
+        :param product_price: Цена товара
+        :param quantity_goods: каоличество товара в магазине
+        """
         self.product = product
         self.product_price = product_price
         self.quantity_goods = quantity_goods
         self.all.append(self)
 
     def price_product_in_store(self):
+        """
+        подсчет общей стоимости оставшегося товара в магазине
+        :return: Общая стоимость
+        """
         return self.product_price * self.quantity_goods
 
     def price_product_discount(self):
+        """
+        Подсчет скидки на одну еденицу товар
+        :return: Цена товара с учетом скидки
+        """
         self.product_price = self.product_price * self.discount
 
 
