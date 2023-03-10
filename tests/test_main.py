2807d27398b577869ee9bef878f0701a77368e4b
andre059 <andreyperm159@mail.ru>
2023-03-10 15:12:14 +0500

    тесты 1

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,16 +2,10 @@ from main import Product
 
 
 def test_price_product_in_store():
-    assert Product.price_product_in_store(10000, 20) == 2000000
-    # assert entering_data.price_product_in_store(-10000, 20) == -2000000
-    # assert entering_data.price_product_in_store(-10000, -20) == 2000000
-    # assert entering_data.price_product_in_store(-10000, 0) == 0
-    # assert entering_data.price_product_in_store(-10000, 'try') is None
+    total_balance = Product("Смартфон", 10000, 20)
+    assert total_balance.price_product_in_store() == 200000
 
 
 def test_price_product_discount():
-    assert Product.price_product_discount(10000, 0.8) == 8000.0
-    # assert price_product_discount(10000, -0.8) == -8000.0
-    # assert price_product_discount(10000, 0) == 0
-    # assert price_product_discount(10000, 1) == 10000
-    # assert price_product_discount(10000, 'try') is None
+    discount = Product("Смартфон", 10000, 0.8)
+    assert discount.price_product_discount() is None
